8a38a719c3d616fd9c04624700713092e04f8ee6
Jeidsan A. da C. Pereira <jeidsan.pereira@gmail.com>
2026-02-17 22:21:45 -0300

    Adicionadas migrations para as tabelas de usuários, permissões e departamentos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/email', function() {
+    Mail::raw('Mensagem de teste do RH MANGNT', function(Message $message) {
+        $message->to('teste@gmail.com')
+                ->subject('Teste de email do RH MANGNT')
+                ->from('rh@rh-mangnt.com.br');
+    });
+
+    echo 'Email enviado com sucesso!';
+});
