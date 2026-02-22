5638b28eb7a54f88b7d09c2276f802deafe69ec6
Jeidsan A. da C. Pereira <jeidsan.pereira@gmail.com>
2026-02-22 10:29:57 -0300

    Ajustes gerais na edição de dados do colaborador

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -6,9 +6,9 @@
         <hr />
         <div class="container-fluid m-0 p-0 mt-5">
             <div class="row">
-                <x-profile-user-change-component />
-                <x-profile-user-change-data />
-                <x-profile-user-change-address />
+                <x-profile-user-change-password />
+                <x-profile-user-change-data :colaborator="$colaborator" />
+                <x-profile-user-change-address :colaborator="$colaborator" />
             </div>
         </div>
     </div>
